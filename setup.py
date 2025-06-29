e1c2c6c983972f9cf02cf9bd2c19e71558fe4eef
Lamartine587 <kipkoechlamartine@gmail.com>
2025-06-29 09:43:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='Secrets',
-    version='',
-    packages=[''],
-    url='',
-    license='',
-    author='don',
-    author_email='',
-    description=''
+    version='1.0.0',
+    py_modules=['app'],
+    url='https://github.com/lamartine587/Secrets',
+    license='MIT',
+    author='Don',
+    author_email='kipkoechlamartine@gmail.com',  # Replace with your email
+    description='A Tkinter-based encryption app with a neon hacker-style interface',
+    install_requires=[
+        'cryptography>=36.0.0',
+    ],
+    python_requires='>=3.8',  # Specify minimum Python version
+    entry_points={
+        'console_scripts': [
+            'neoncrypt=app:main',
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
 )
